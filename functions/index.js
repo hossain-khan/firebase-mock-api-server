c270a5f26a4cc3f2bbcc57e49c835518cc975e75
Hossain Khan <amardeshbd@gmail.com>
2019-03-12 18:08:27 -0400

    [ADDED] Express JS with sample API function Ref: https://github.com/firebase/functions-samples/tree/Node-8/quickstarts/big-ben

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// [START import]
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+// [END import]
+
+// [START middleware]
+const cors = require('cors')({origin: true});
+app.use(cors);
+// [END middleware]
+
+
+/**
+ * Say hello as response
+ * ----------------------------------------
+ */
+app.get('/say/hello', function(req, res) {
+   console.log('Request Query Params: ', req.query);
+
+  // Success for any other query
+  return res.status(200)
+            .json({"message":"Hello there... Welcome to mock server."});
+});
+/* [END `/say/hello` ] */
+
+// [START export]
+// Export the express app as an HTTP Cloud Functions as `api` function
+exports.api = functions.https.onRequest(app);
+// [END export]
