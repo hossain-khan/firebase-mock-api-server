147d4d7b3b6a84a0e77841fbaec13f034ad808d8
Hossain Khan <amardeshbd@gmail.com>
2019-03-12 18:28:14 -0400

    [ADDED] Another simple API to showcase PATH param

--- functions/index.js
+++ functions/index.js
@@ -25,6 +25,27 @@ app.get('/say/hello', function(req, res) {
 });
 /* [END `/say/hello` ] */
 
+
+/**
+ * Provides user's information based on user ID
+ * Captures dynamic params from URL (See https://expressjs.com/en/guide/routing.html)
+ * ----------------------------------------
+ */
+app.get('/users/:userId', function (req, res) {
+  console.log('Request Query Params: ', req.query);
+  console.log('Request URL Params: ', req.params);
+
+  if(!req.params.hasOwnProperty('userId')) {
+    return res.status(400)
+          .json({"message":"The `userId` is required."});
+  }
+
+  return res.status(200)
+            .json({"userId": req.params.userId, "name": "Demo User"})
+});
+/* [END `/users/:userId` ] */
+
+
 // [START export]
 // Export the express app as an HTTP Cloud Functions as `api` function
 exports.api = functions.https.onRequest(app);
